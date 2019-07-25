b25afbee53d47248de822c20c722754b3a7f942f
Rupalipatil1001 <rupalipatil1001@gmail.com>
2019-07-25 11:01:36 +0530

    Initial commit

---
+++ EventAPI/Controllers/EventsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using EventAPI.Infrastructure;
+using EventAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventAPI.Controllers
+{  // [EnableCors("MSPolicy")]
+    [Produces("text/json","text/xml")]
+    [Route("api/[controller]")]//route prefix
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private EventDbContext db;
+
+        public EventsController(EventDbContext dbContext)
+        {
+            db = dbContext;
+        }
+
+        //GET/api/events
+        [HttpGet(Name ="GetAll")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public ActionResult<List<EventInfo>> GetEvents()
+        {
+            var events = db.Events.ToList();
+            return Ok(events); //returns with status code 200
+        }
+
+        //POST/api/events
+        [HttpPost(Name ="AddEvent")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [Authorize]
+        public async Task<ActionResult<EventInfo>>AddEventAsync([FromBody]EventInfo eventInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = db.Events.Add(eventInfo);
+                await db.SaveChangesAsync();
+                //  return CreatedAtAction("GetById", new { id = result.Entity.id }, result.Entity);
+                return CreatedAtAction(nameof(GetEventAsync), new { id = result.Entity.id }, result.Entity);//returns the status code 201
+                                                                                                            //  return Created("", result.Entity);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        //GET/api/events/{id}
+        [HttpGet("{id}", Name ="GetById")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+
+        public async Task<ActionResult<EventInfo>> GetEventAsync([FromRoute] int id)
+        {
+           // throw new Exception();
+            var eventInfo = await db.Events.FindAsync(id);
+            if (eventInfo != null)
+                return Ok(eventInfo);
+            else
+            return NotFound("Item you are searching not found");
+        }
+    }
+}
--- EventAPI/Startup.cs
+++ EventAPI/Startup.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using EventAPI.CustomFilters;
+using EventAPI.CustomFormatters;
+using EventAPI.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace EventAPI
 {
@@ -24,7 +33,76 @@ namespace EventAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<EventDbContext>(options =>
+            {
+                //  options.UseInMemoryDatabase(databaseName: "EventDb");
+                options.UseSqlServer(Configuration.GetConnectionString("EventSqlConnection"));
+                
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title="Event API",
+                    Version="v1",
+                    Contact=new Contact { Name="Sonu Sathyadas", Email="Sonu@hotmail.com"}
+                });
+            });
+            services.AddCors(c =>
+            {
+                c.AddPolicy("MSPolicy", builder =>
+                 {
+                     builder.WithOrigins("*.microsoft.com")
+                     .AllowAnyMethod()
+                     .AllowAnyHeader();
+                 });
+                c.AddPolicy("SynPolicy", builder =>
+                {
+                    builder.WithOrigins("*.synergetics-india.com")
+                    .WithMethods("GET")
+                    .WithHeaders("Authorization", "Content-Type","Accept");
+
+                });
+
+                c.AddPolicy("Others", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+
+                });
+
+                c.AddPolicy("SynPolicy", builder =>
+                {
+                    builder.WithOrigins("*.synergetics-india.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();                   
+
+                });
+                c.DefaultPolicyName = "Others";
+            }
+                );
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration.GetValue<string>("Jwt:Issuer"),
+                        ValidAudience = Configuration.GetValue<string>("Jwt:Audience"),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Jwt:Secret")))
+                    };
+                });
+            services.AddMvc(c =>
+            {
+                c.Filters.Add(typeof(CustomExceptionHandler));
+                c.OutputFormatters.Add(new CsvCustomFormatter());
+            })
+                .AddXmlDataContractSerializerFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +112,28 @@ namespace EventAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.UseCors(c=>
+            //{
+            //    c.WithOrigins("*.microsoft.com")
+            //    .AllowAnyMethod()
+            //    .AllowAnyHeader();
 
+            //    c.WithOrigins("*.synergetics-india.com")
+            //    .WithMethods("GET")
+            //    .WithHeaders("Authorisation", "Content-Type", "Accept");
+            //});
+
+            app.UseCors("Others");
+            app.UseAuthentication();
+
+            app.UseSwagger();
+            if(env.IsDevelopment())
+            {
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API");
+                });
+            }
             app.UseMvc();
         }
     }
